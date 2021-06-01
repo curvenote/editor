ff41e7bc867a97b0f24c1ea7d01f4586f900b032
Rowan Cockett <rowanc1@gmail.com>
2021-06-01 08:11:37 -0600

    🚲  Simplify CiteView

--- src/views/CiteView.tsx
+++ src/views/CiteView.tsx
@@ -1,48 +1,26 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import classNames from 'classnames';
+/* eslint-disable max-classes-per-file */
 import { Node } from 'prosemirror-model';
 import { EditorView } from 'prosemirror-view';
-import { useCitation } from '../components/Citation';
-import { State } from '../store/types';
-import { getEditorUI } from '../store/selectors';
-import createNodeView from './NodeView';
 
+class CiteView {
+  // The node's representation in the editor (empty, for now)
+  dom: HTMLElement;
 
-type Props = {
-  view: EditorView;
   node: Node;
-  open: boolean;
-  edit: boolean;
-};
 
-const CiteInline = (props: Props) => {
-  const {
-    view, node, open, edit,
-  } = props;
-  const uid = node.attrs.key;
-  const {
-    loading, inline, json, error,
-  } = useCitation(uid);
+  view: EditorView;
 
-  const selected = useSelector((state: State) => getEditorUI(state).viewId === view.dom.id);
+  getPos?: () => number;
 
-  const filler = loading ? 'Loading Citation, 0000' : 'Citation Not Found';
-
-  return (
-    <span
-      className={classNames({ 'ProseMirror-selectednode': selected && open && edit, error: !loading && error })}
-      title={json?.title ?? ''}
-      key={uid}
-    >
-      {inline || filler}
-    </span>
-  );
-};
-
-const CiteView = createNodeView(
-  CiteInline,
-  { wrapper: 'span', className: 'citation' },
-);
+  constructor(node: Node, view: EditorView, getPos: (() => number) | undefined) {
+    this.node = node;
+    this.view = view;
+    this.getPos = getPos;
+    this.dom = document.createElement<'cite'>('cite');
+    const { title, inline } = node.attrs;
+    this.dom.setAttribute('title', title);
+    this.dom.textContent = inline;
+  }
+}
 
 export default CiteView;
