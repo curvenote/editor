ff2ae69eaf111abafec63f47598580093c7cd240
Rowan Cockett <rowanc1@gmail.com>
2021-06-22 14:07:47 -0600

    🏷  Remove label component

--- src/components/InlineActions/AlignActions.tsx
+++ src/components/InlineActions/AlignActions.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { makeStyles, createStyles, Grid } from '@material-ui/core';
 import { isNodeSelection } from 'prosemirror-utils';
 import { NodeSelection } from 'prosemirror-state';
@@ -7,11 +7,9 @@ import { types } from '@curvenote/schema';
 import MenuIcon from '../Menu/Icon';
 import { deleteNode, updateNodeAttrs } from '../../store/actions';
 import SelectWidth from './SelectWidth';
-import TextAction from './TextAction';
 import { ActionProps, positionPopper } from './utils';
 import { Dispatch, State } from '../../store';
 import { getEditorState } from '../../store/selectors';
-import { createId } from '../../utils';
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -31,13 +29,10 @@ const AlignActions: React.FC<Props> = (props) => {
   const { stateId, viewId, anchorEl, showCaption } = props;
   const dispatch = useDispatch<Dispatch>();
   const classes = useStyles();
-  const [labelOpen, setLabelOpen] = useState(false);
   const selection = useSelector((state: State) => getEditorState(state, stateId)?.state?.selection);
   const { node, from: pos } = (selection as NodeSelection) ?? {};
-  // If the node changes, set open label to false
-  useEffect(() => setLabelOpen(false), [node]);
   if (!node || !selection || !isNodeSelection(selection)) return null;
-  const { align, width, numbered, caption, label } = node?.attrs;
+  const { align, width, numbered, caption } = node?.attrs;
 
   const onAlign = (a: types.AlignOptions) => () => {
     dispatch(updateNodeAttrs(stateId, viewId, { node, pos }, { align: a }));
@@ -46,58 +41,13 @@ const AlignActions: React.FC<Props> = (props) => {
     dispatch(updateNodeAttrs(stateId, viewId, { node, pos }, { width: value }));
   };
   const onNumbered = () =>
-    dispatch(
-      updateNodeAttrs(
-        stateId,
-        viewId,
-        { node, pos },
-        label === '' ? { numbered: !numbered, label: createId('fig') } : { numbered: !numbered },
-      ),
-    );
+    dispatch(updateNodeAttrs(stateId, viewId, { node, pos }, { numbered: !numbered }));
   const onCaption = () =>
-    dispatch(
-      updateNodeAttrs(
-        stateId,
-        viewId,
-        { node, pos },
-        label === '' && !caption
-          ? { caption: !caption, label: createId('fig') }
-          : { caption: !caption },
-      ),
-    );
-  const onLabel = (t: string) =>
-    dispatch(
-      updateNodeAttrs(
-        stateId,
-        viewId,
-        { node, pos },
-        t === '' ? { label: createId('fig') } : { label: t },
-      ),
-    );
+    dispatch(updateNodeAttrs(stateId, viewId, { node, pos }, { caption: !caption }));
+
   const onDelete = () => dispatch(deleteNode(stateId, viewId, { node, pos }));
 
-  const validateId = async (t: string) => {
-    if (t === '') return true;
-    const r = /^([a-zA-Z][a-zA-Z0-9-]*[a-zA-Z0-9])$/;
-    return r.test(t);
-  };
-
   positionPopper(anchorEl);
-
-  if (labelOpen) {
-    return (
-      <TextAction
-        text={label}
-        onCancel={() => setLabelOpen(false)}
-        onSubmit={(t) => {
-          onLabel(t);
-          setLabelOpen(false);
-        }}
-        validate={validateId}
-        help="The ID must be at least two characters and start with a letter, it may have dashes inside."
-      />
-    );
-  }
 
   return (
     <Grid container alignItems="center" justify="center" className={classes.root}>
@@ -110,12 +60,7 @@ const AlignActions: React.FC<Props> = (props) => {
         <>
           <MenuIcon kind="divider" />
           <MenuIcon kind="caption" active={caption} onClick={onCaption} />
-          {caption && (
-            <>
-              <MenuIcon kind="label" active={Boolean(label)} onClick={() => setLabelOpen(true)} />
-              <MenuIcon kind="numbered" active={numbered} onClick={onNumbered} />
-            </>
-          )}
+          {caption && <MenuIcon kind="numbered" active={numbered} onClick={onNumbered} />}
         </>
       )}
       <MenuIcon kind="divider" />
--- src/components/InlineActions/HeadingActions.tsx
+++ src/components/InlineActions/HeadingActions.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback } from 'react';
 import { makeStyles, createStyles, Grid, Menu } from '@material-ui/core';
 import { Node } from 'prosemirror-model';
 import { schemas } from '@curvenote/schema';
@@ -7,7 +7,6 @@ import { NodeSelection } from 'prosemirror-state';
 import { useDispatch, useSelector } from 'react-redux';
 import MenuIcon from '../Menu/Icon';
 import { updateNodeAttrs, wrapInHeading } from '../../store/actions';
-import TextAction from './TextAction';
 import { getEditorState } from '../../store/selectors';
 import { Dispatch, State } from '../../store';
 import { ActionProps, positionPopper } from './utils';
@@ -30,14 +29,12 @@ const HeadingActions: React.FC<ActionProps> = (props) => {
   const { stateId, viewId } = props;
   const classes = useStyles();
   const dispatch = useDispatch<Dispatch>();
-  const [labelOpen, setLabelOpen] = useState(false);
   const state = useSelector((s: State) => getEditorState(s, stateId)?.state);
   const parent =
     state?.selection &&
     findParentNode((n: Node) => n.type.name === schemas.nodeNames.heading)(state?.selection);
   const node = parent?.node ?? (state?.selection as NodeSelection).node;
   const pos = parent?.pos ?? state?.selection?.from;
-  useEffect(() => setLabelOpen(false), [node]);
 
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
 
@@ -49,7 +46,7 @@ const HeadingActions: React.FC<ActionProps> = (props) => {
 
   if (!node || pos == null) return null;
   // If the node changes, set open label to false
-  const { numbered, label, level } = node.attrs;
+  const { numbered, level } = node.attrs;
 
   const onNumbered = () =>
     dispatch(updateNodeAttrs(stateId, viewId, { node, pos }, { numbered: !numbered }, false));
@@ -63,32 +60,8 @@ const HeadingActions: React.FC<ActionProps> = (props) => {
     // This maintains the section label/number info
     dispatch(updateNodeAttrs(stateId, viewId, { node, pos }, { level: l }, false));
   };
-  const onLabel = (t: string) =>
-    dispatch(updateNodeAttrs(stateId, viewId, { node, pos }, { label: t }, false));
 
-  const validateId = async (t: string) => {
-    if (t === '') return true;
-    const r = /^([a-zA-Z][a-zA-Z0-9-]*[a-zA-Z0-9])$/;
-    return r.test(t);
-  };
-
-  // Reposition the popper
   positionPopper();
-
-  if (labelOpen) {
-    return (
-      <TextAction
-        text={label}
-        onCancel={() => setLabelOpen(false)}
-        onSubmit={(t) => {
-          onLabel(t);
-          setLabelOpen(false);
-        }}
-        validate={validateId}
-        help="The ID must be at least two characters and start with a letter, it may have dashes inside."
-      />
-    );
-  }
 
   return (
     <Grid container alignItems="center" justify="center" className={classes.root}>
@@ -127,7 +100,6 @@ const HeadingActions: React.FC<ActionProps> = (props) => {
           <Keyboard shortcut="Mod-Alt-6" />
         </MenuAction>
       </Menu>
-      <MenuIcon kind="label" active={Boolean(label)} onClick={() => setLabelOpen(true)} />
       <MenuIcon kind="numbered" active={numbered} onClick={onNumbered} />
     </Grid>
   );
