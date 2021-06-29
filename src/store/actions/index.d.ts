f3fe3a4f23882ad7f7320a6dad128836b2656e64
rowanc1 <rowanc1@users.noreply.github.com>
2021-06-29 21:35:48 +0000

    deploy: 55732e5977e16818fb39e8f2d2f3bb1cd4c2a8f6

--- src/store/actions/index.d.ts
+++ src/store/actions/index.d.ts
@@ -1,6 +1,7 @@
 export * from '../state/actions';
 export * from '../attrs/actions';
 export * from '../ui/actions';
+export * from '../ui/utils';
 export * from '../suggestion/actions';
 export * from './editor';
 export * from './utils';
