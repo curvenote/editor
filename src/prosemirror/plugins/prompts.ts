506d9527a11cdc0afb5a604f9082c5cb99b614f4
Rowan Cockett <rowanc1@gmail.com>
2021-07-26 12:51:39 -0600

    🎲  Simplify prompt code

--- src/prosemirror/plugins/prompts.ts
+++ src/prosemirror/plugins/prompts.ts
@@ -6,34 +6,32 @@ import { isEditable } from './editable';
 
 export const key = new PluginKey('prompt');
 
-export interface PromptState {
-  prompt: DecorationSet;
-}
+const getParentIfParagraph = findParentNode(
+  (node) => node.type.name === schemas.nodeNames.paragraph,
+);
 
-const getPromptPlugin = (): Plugin<PromptState> => {
-  const promptPlugin: Plugin<PromptState> = new Plugin({
+const getPromptPlugin = (): Plugin<DecorationSet> => {
+  const promptPlugin: Plugin<DecorationSet> = new Plugin({
     key,
     state: {
-      init: () => ({ prompt: DecorationSet.empty } as PromptState),
-      apply(tr, value, oldState, newState): PromptState {
-        const getParagraph = findParentNode(
-          (node) => node.type.name === schemas.nodeNames.paragraph,
-        );
-        const para = getParagraph(tr.selection);
-        const editable = isEditable(newState);
-        if (editable && tr.selection.empty && para && para.node.nodeSize === 2) {
+      init: () => DecorationSet.empty,
+      apply(tr, value: DecorationSet, oldState, newState): DecorationSet {
+        if (!isEditable(newState)) return DecorationSet.empty;
+        const paragraph = getParentIfParagraph(newState.selection);
+        const emptyParagraph = paragraph && paragraph.node.nodeSize === 2;
+        if (tr.selection.empty && emptyParagraph) {
           const deco = Decoration.node(tr.selection.from - 1, tr.selection.to + 1, {
             class: 'prompt',
           });
-          return { prompt: new DecorationSet().add(tr.doc, [deco]) };
+          return DecorationSet.create(tr.doc, [deco]);
         }
-        return {
-          prompt: DecorationSet.empty,
-        };
+        return DecorationSet.empty;
       },
     },
     props: {
-      decorations: (state) => promptPlugin.getState(state).prompt,
+      decorations(state) {
+        return this.getState(state);
+      },
     },
   });
   return promptPlugin;
