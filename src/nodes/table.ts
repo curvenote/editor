7d7c406a251fb5ca0e748851a85c5e370af161ab
Yuxi Wang <yuxi.wangcd@gmail.com>
2021-09-16 14:49:24 -0600

    Add toMarkdown in tables

--- src/nodes/table.ts
+++ src/nodes/table.ts
@@ -19,7 +19,43 @@ export const nodes = tableNodes({
 });
 
 export const toMarkdown: FormatSerialize = (state, node) => {
-  console.log('table markdown', node.attrs);
-  state.write('');
+  let mdStr = '';
+
+  node.content.forEach((child) => {
+    if (child.type.name === 'table_row') {
+      let isHeader = false;
+      mdStr += '|';
+      child.content.forEach((cell) => {
+        if (cell.type.name === 'table_header') {
+          isHeader = true;
+        }
+        if (cell.type.name === 'table_cell' || cell.type.name === 'table_header') {
+          mdStr += ' ';
+          // state.renderInline(cell);
+          if (Number(cell.attrs.colspan) > 1) {
+            mdStr += `${cell.textContent} |`.repeat(Number(cell.attrs.colspan));
+          } else {
+            mdStr += cell.textContent;
+            mdStr += ' |';
+          }
+        }
+      });
+      mdStr += '\n';
+      if (isHeader) {
+        mdStr += '|';
+        child.content.forEach((cell) => {
+          if (cell.type.name === 'table_header') {
+            mdStr += '---|';
+          }
+        });
+        mdStr += '\n';
+      }
+    }
+  });
+  state.write(mdStr);
+  state.ensureNewLine();
 };
-export const toTex: FormatSerialize = (state, node) => {};
+export const toTex: FormatSerialize = (state, node) => {
+  console.log('table toTex', node);
+  state.write(`{\\bf \`${node.type.name}' not supported in \\LaTeX}`);
+};
