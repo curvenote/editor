0f0cf504a277a620cd5e1dd8e448d1f80e07ed6e
Rowan Cockett <rowanc1@gmail.com>
2021-10-22 13:26:22 -0600

    💥  Figure/Image inline actions

--- src/components/InlineActions/FigureActions.tsx
+++ src/components/InlineActions/FigureActions.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { makeStyles, createStyles, Grid } from '@material-ui/core';
-import { Node } from 'prosemirror-model';
+import { Node, Schema } from 'prosemirror-model';
 import { findChildrenByType, findParentNode } from 'prosemirror-utils';
 import { useDispatch, useSelector } from 'react-redux';
-import { nodeNames, Nodes, types } from '@curvenote/schema';
+import { nodeNames, Nodes, types, CaptionKind } from '@curvenote/schema';
 import { NodeSelection, TextSelection } from 'prosemirror-state';
 import MenuIcon from '../Menu/Icon';
 import { applyProsemirrorTransaction, deleteNode, updateNodeAttrs } from '../../store/actions';
@@ -26,6 +26,19 @@ const useStyles = makeStyles(() =>
 
 type Props = ActionProps;
 
+export function createFigureCaption(schema: Schema, src: string) {
+  const FigcaptionNode = schema.nodes[nodeNames.figcaption];
+  const fragment = opts.getCaptionFragment(schema, src);
+  const captionAttrs: Nodes.Figcaption.Attrs = {
+    kind: CaptionKind.fig, // TODO: determine out what kind it is!!
+    id: createId(),
+    label: null,
+    numbered: true,
+  };
+  const caption = FigcaptionNode.create(captionAttrs, fragment);
+  return caption;
+}
+
 function toggleCaption(stateId: any, viewId: string | null, figurePos: number): AppThunk {
   return (dispatch, getState) => {
     const editorState = getEditorState(getState(), stateId)?.state;
@@ -40,17 +53,25 @@ function toggleCaption(stateId: any, viewId: string | null, figurePos: number): 
     // The inside of a non-leaf node is always node.pos + 1
     const start = figurePos + 1;
     if (figcaption) {
+      // Delete figure caption
+      if (figure.childCount === 1) {
+        // If there is only a figure caption, delete the entire node
+        const nodeSelection = NodeSelection.create(editorState.doc, figurePos);
+        const tr = editorState.tr.setSelection(nodeSelection).deleteSelection();
+        dispatch(applyProsemirrorTransaction(stateId, viewId, tr, true));
+        return;
+      }
       const nodeSelection = NodeSelection.create(editorState.doc, start + figcaption.pos);
       const tr = editorState.tr.setSelection(nodeSelection).deleteSelection();
-      const selected = tr
-        .setSelection(NodeSelection.create(tr.doc, start + image.pos)) // this assumes figcaption comes after image
-        .scrollIntoView();
+      let selected = tr;
+      if (image) {
+        selected = tr
+          .setSelection(NodeSelection.create(tr.doc, start + image.pos))
+          .scrollIntoView();
+      }
       dispatch(applyProsemirrorTransaction(stateId, viewId, selected, true));
     } else {
-      const fragment = opts.getCaptionFragment(editorState.schema, image.node.attrs.src);
-      // TODO: put a kind in?
-      const captionAttrs: Nodes.Figcaption.Attrs = { id: createId(), label: null, numbered: true };
-      const newcaption = FigcaptionNode.create(captionAttrs, fragment);
+      const newcaption = createFigureCaption(editorState.schema, image.node.attrs.src);
       const insertion = start + image.pos + image.node.nodeSize;
       const tr = editorState.tr.insert(insertion, newcaption);
       const captionstart = insertion + 1;
@@ -77,18 +98,18 @@ const FigureImageActions: React.FC<Props> = (props) => {
   if (!editorState || !figure || pos == null) return null;
   const { align } = figure?.attrs as Nodes.Figure.Attrs;
   const image = findChildrenByType(figure, editorState.schema.nodes[nodeNames.image])[0];
-  if (!image) return null;
   const FigcaptionNode = editorState.schema.nodes[nodeNames.figcaption];
   const figcaption = findChildrenByType(figure, FigcaptionNode)[0]; // Note: this may be undefined!!
 
   const onAlign = (a: types.AlignOptions) => () => {
     dispatch(updateNodeAttrs(stateId, viewId, { node: figure, pos }, { align: a }));
   };
-  image.pos = pos + 1 + image.pos;
+  if (image) image.pos = pos + 1 + image.pos;
   if (figcaption) figcaption.pos = pos + 1 + figcaption.pos;
-  const { width } = image.node.attrs as Nodes.Image.Attrs;
+  const width = (image?.node.attrs as Nodes.Image.Attrs)?.width ?? null;
   const numbered = (figcaption?.node.attrs as Nodes.Figcaption.Attrs)?.numbered ?? false;
   const onWidth = (value: number) => {
+    if (!image) return;
     dispatch(updateNodeAttrs(stateId, viewId, image, { width: value }));
     positionPopper();
   };
