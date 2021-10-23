0a1e04152b9e1c6f20aa7d3898743576dce2e98d
Rowan Cockett <rowanc1@gmail.com>
2021-10-22 18:02:39 -0600

    💥 Linting and figurecaption enter at top

--- src/prosemirror/keymap/figure.ts
+++ src/prosemirror/keymap/figure.ts
@@ -5,12 +5,12 @@ import { findParentNodeOfType } from 'prosemirror-utils';
 import { EditorView } from 'prosemirror-view';
 import { KeyMap } from './types';
 
-function insertParagraphAndSelect(view: EditorView, side: number, out: number = 0) {
+function insertParagraphAndSelect(view: EditorView, side: number) {
   const transaction = view.state.tr.insert(
     side,
     view.state.schema.nodes[nodeNames.paragraph].createAndFill(),
   );
-  transaction.setSelection(TextSelection.create(transaction.doc, side + out));
+  transaction.setSelection(TextSelection.create(transaction.doc, side + 1));
   return transaction;
 }
 
@@ -47,8 +47,7 @@ export const handleBackspace: KeyMap = function handleBackspace(
       return false;
     }
 
-    if (imageIndex === -1) {
-    } else {
+    if (imageIndex !== -1) {
       if (captionIndex > -1) {
         const start = $head.start();
         if (captionIndex > imageIndex) {
@@ -80,58 +79,59 @@ export const handleEnterCommand: KeyMap = function handleEnterCommand(
   view?: EditorView,
 ) {
   const { $head } = state.selection;
-  if ($head.parent.type.name === nodeNames.figcaption) {
-    const figcaption = $head.parent;
-    const figure = $head.node($head.depth - 1);
-    if (figure.type.name !== nodeNames.figure) {
-      return false;
+  if ($head.parent.type.name !== nodeNames.figcaption) return false;
+  // We are in a figure caption!!
+  const figcaption = $head.parent;
+  const figure = $head.node($head.depth - 1);
+  if (figure.type.name !== nodeNames.figure) {
+    return false;
+  }
+
+  let imageIndex = -1;
+  let captionIndex = -1;
+
+  figure.forEach((child, _offset, i) => {
+    if (child.type.name === nodeNames.image) {
+      imageIndex = i;
     }
-
-    let imageIndex = -1;
-    let captionIndex = -1;
-
-    figure.forEach((child, _offset, i) => {
-      if (child.type.name === nodeNames.image) {
-        imageIndex = i;
-      }
-      if (child.eq(figcaption)) {
-        captionIndex = i;
-      }
-    });
-
-    if (!view || !dispatch) {
-      return false;
+    // TODO: if the figure caption is the same this is not going to work!!
+    if (child.eq(figcaption)) {
+      captionIndex = i;
     }
+  });
 
-    if (imageIndex === -1) {
-    } else {
-      if (captionIndex > -1) {
-        const start = $head.start();
-        const end = $head.end();
-        const depth = $head.depth;
-        if (captionIndex > imageIndex) {
-          // when caption is below the image
-          if (end === $head.pos) {
-            // selection is at the end of the caption, we create a new paragraph below
-            view.dispatch(insertParagraphAndSelect(view, end + depth, 1));
-          } else {
-            selectParent(view, start);
-          }
-          return true;
+  if (!view || !dispatch) {
+    return false;
+  }
+
+  // TODO: I think this should work regardless of image.
+  // TODO: this needs to also work with iframe, table... code...
+  // TODO: maybe parentOffset of the caption??
+  if (imageIndex !== -1) {
+    if (captionIndex > -1) {
+      const start = $head.start();
+      const end = $head.end();
+      const { depth } = $head;
+      if (captionIndex > imageIndex) {
+        // when caption is below the image
+        if (end === $head.pos) {
+          // selection is at the end of the caption, we create a new paragraph below
+          view.dispatch(insertParagraphAndSelect(view, end + depth));
         } else {
-          // when caption is above the image
-          if ($head.pos === start) {
-            view.dispatch(insertParagraphAndSelect(view, start - depth));
-          } else {
-            selectParent(view, start);
-          }
-          return true;
+          selectParent(view, start);
         }
-      } else {
-        // shouldn't reach here since caption should exsits in the figure
-        return false;
+        return true;
       }
+      // when caption is above the image
+      if ($head.pos === start) {
+        view.dispatch(insertParagraphAndSelect(view, start - depth));
+      } else {
+        selectParent(view, start);
+      }
+      return true;
     }
+    // shouldn't reach here since caption should exsits in the figure
+    return false;
   }
   return false;
 };
