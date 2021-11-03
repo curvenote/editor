b00fb2896c7463cb1e06c9847d0d57b3a066bc61
Yuxi Wang <yuxi.wangcd@gmail.com>
2021-11-03 10:22:45 -0700

    Export MentionInput

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export { default as Editor } from './Editor';
 
 export { default as EditorMenu } from './Menu';
+export { default as MentionInput } from './MentionInput';
 export { default as MenuIcon } from './Menu/Icon';
 
 export * from './InlineActions';
