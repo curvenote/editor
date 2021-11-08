2336326aea9e0acb341344877282d72bbf3329dc
Yuxi Wang <yuxi.wangcd@gmail.com>
2021-11-08 16:59:19 -0700

    Add email invite suggestion

--- demo/components.tsx
+++ demo/components.tsx
@@ -1,6 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import MentionInput, { PersonSuggestion } from '../src/components/MentionInput';
+
+export const EmailAvatar =
+  'data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiBox-root css-1om0hkc" focusable="false" viewBox="0 0 24 24" aria-hidden="true" data-testid="EmailOutlinedIcon"%3E%3Cpath d="M22 6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6zm-2 0-8 5-8-5h16zm0 12H4V8l8 5 8-5v10z"%3E%3C/path%3E%3C/svg%3E';
+
+function validateEmail(email: string) {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
 
 const TEST_SUGGESTION_LIST: PersonSuggestion[] = [
   { avatar: '', email: 'test3@gmail.com', name: '', id: 'test-1' },
@@ -49,21 +58,32 @@ const INITIAL_MENTION: PersonSuggestion[] = [
 ];
 
 function ComponentDemo() {
-  const [suggestions, setSuggestion] = useState(TEST_SUGGESTION_LIST);
+  const [isEmail, setIsEmail] = useState(false);
+  const [suggestions, setSuggestions] = useState(TEST_SUGGESTION_LIST);
+  const [search, setSearch] = useState('');
   const [mentions, setMentions] = useState(INITIAL_MENTION);
+
+  useEffect(() => {
+    if (validateEmail(search)) {
+      setIsEmail(true);
+    } else {
+      setIsEmail(false);
+    }
+  }, [search]);
   return (
     <div>
       <MentionInput
-        suggestions={suggestions}
+        suggestions={
+          isEmail ? [{ email: search, name: search, avatar: EmailAvatar, id: search }] : suggestions
+        }
         onChange={(v) => {
-          console.log('oncHnage', v);
           setMentions(v);
         }}
         onSearchChanged={(update) => {
           if (!update) {
             return;
           }
-          console.log('onSearchChanged', update);
+          setSearch(update);
         }}
       />
       <pre>{JSON.stringify(mentions, null, 2)}</pre>
