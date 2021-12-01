399c0962437178849102abf9b41f042a7dc22ea4
Yuxi Wang <yuxi.wangcd@gmail.com>
2021-12-01 15:57:51 -0800

    Improve Mention styles

--- src/views/Mention.tsx
+++ src/views/Mention.tsx
@@ -3,21 +3,23 @@ import ReactDOM from 'react-dom';
 import { EditorView } from 'prosemirror-view';
 import { Node } from 'prosemirror-model';
 import CloseIcon from '@material-ui/icons/Close';
-import { Chip } from '@material-ui/core';
-import PersonIcon from '@material-ui/icons/Person';
+import { Chip, makeStyles, createStyles } from '@material-ui/core';
 
 export type MentionResult = { id?: string; name?: string; email?: string; error?: boolean };
 
-function Mention({ label, user, onDelete }: { label: string; user: string; onDelete: () => void }) {
-  return (
-    <Chip
-      icon={<PersonIcon />}
-      label={`${label} user - ${user}`}
-      variant="outlined"
-      onDelete={onDelete}
-      deleteIcon={<CloseIcon />}
-    />
-  );
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      backgroundColor: '#e0e0e0',
+      borderRadius: '10px',
+      padding: '2px 4px',
+    },
+  }),
+);
+
+function Mention({ label, user }: { label: string; user: string }) {
+  const classes = useStyles();
+  return <span className={classes.root}>{`${label} user - ${user}`}</span>;
 }
 
 export default class MentionView {
@@ -34,22 +36,7 @@ export default class MentionView {
     this.view = view;
     this.getPos = getPos;
     const wrapper = document.createElement('span');
-    ReactDOM.render(
-      <Mention
-        onDelete={() => {
-          const {
-            state: { tr },
-            dispatch,
-          } = this.view;
-          if (typeof getPos === 'boolean') return;
-          const pos = getPos();
-          dispatch(tr.delete(pos, pos + node.nodeSize));
-        }}
-        label={node.attrs.label}
-        user={node.attrs.user}
-      />,
-      wrapper,
-    );
+    ReactDOM.render(<Mention label={node.attrs.label} user={node.attrs.user} />, wrapper);
     this.dom = wrapper;
   }
 }
