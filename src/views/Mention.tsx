f48d6631f6f0265ccf54ddd5e2ed0ee37ed12fac
Yuxi Wang <yuxi.wangcd@gmail.com>
2021-12-02 10:46:07 -0800

    Update MentionView with simplied logic

--- src/views/Mention.tsx
+++ src/views/Mention.tsx
@@ -2,8 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
 import { EditorView } from 'prosemirror-view';
 import { Node } from 'prosemirror-model';
-import CloseIcon from '@material-ui/icons/Close';
-import { Chip, makeStyles, createStyles } from '@material-ui/core';
+import { makeStyles, createStyles } from '@material-ui/core';
 
 export type MentionResult = { id?: string; name?: string; email?: string; error?: boolean };
 
@@ -17,47 +16,9 @@ const useStyles = makeStyles(() =>
   }),
 );
 
-function useHover(): [React.MutableRefObject<HTMLElement | null>, boolean] {
-  const [value, setValue] = useState(false);
-  const ref = useRef<HTMLElement | null>(null);
-  const handleMouseOver = () => setValue(true);
-  const handleMouseOut = () => setValue(false);
-  useEffect(
-    () => {
-      const node = ref.current;
-      if (node) {
-        node.addEventListener('mouseover', handleMouseOver);
-        node.addEventListener('mouseout', handleMouseOut);
-        return () => {
-          node.removeEventListener('mouseover', handleMouseOver);
-          node.removeEventListener('mouseout', handleMouseOut);
-        };
-      }
-    },
-    [ref.current], // Recall only if ref changes
-  );
-  return [ref, value];
-}
-
-function Mention({
-  label,
-  user,
-  onHover,
-}: {
-  label: string;
-  user: string;
-  onHover: (user: string, isHover: boolean) => void;
-}) {
+function Mention({ label, user }: { label: string; user: string }) {
   const classes = useStyles();
-  const [ref, isHover] = useHover();
-  useEffect(() => {
-    onHover(user, isHover);
-  }, [isHover, onHover]);
-  return <span ref={ref} className={classes.root}>{`${label} user - ${user}`}</span>;
-}
-
-function onHover(user: string, isHover: boolean) {
-  console.log('hover', { user, isHover });
+  return <span className={classes.root}>{`${label} user - ${user}`}</span>;
 }
 
 export default class MentionView {
@@ -75,10 +36,7 @@ export default class MentionView {
     this.getPos = getPos;
     const wrapper = document.createElement('span');
 
-    ReactDOM.render(
-      <Mention label={node.attrs.label} user={node.attrs.user} onHover={onHover} />,
-      wrapper,
-    );
+    ReactDOM.render(<Mention label={node.attrs.label} user={node.attrs.user} />, wrapper);
     this.dom = wrapper;
   }
 }
