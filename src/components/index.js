30154789630099c03c4e81fb2bda1bc8b5e71cb9
stevejpurves <stevejpurves@users.noreply.github.com>
2021-12-13 14:30:40 +0000

    deploy: 282467047db0ada0b9a9a3d6a4d7f17609f8b50f

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export { default as Editor } from './Editor';
 export { default as EditorMenu } from './Menu';
+export { default as SelectWidth } from './InlineActions/SelectWidth';
 export { default as MenuIcon } from './Menu/Icon';
 export * from './InlineActions';
 export * from './Suggestion';
