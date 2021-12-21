65696ac9cd1b242fb4a9be017459569eb7a29d12
Yuxi Wang <yuxi.wangcd@gmail.com>
2021-12-21 10:55:24 -0800

    Update heading numbered to use data-section

--- src/views/index.ts
+++ src/views/index.ts
@@ -6,12 +6,11 @@ import { TimeView } from './TimeView';
 import { MentionView } from './Mention';
 import { CodeBlockView } from './CodeBlockView';
 import { FootnoteView } from './FootnoteView';
-import HeadingView from './HeaderView';
 import createNodeView from './NodeView';
 import WidgetView, { newWidgetView } from './WidgetView';
 import { clickSelectFigure } from './utils';
 
-export type { NodeViewProps } from './types';
+export type { NodeViewProps, GetPos } from './types';
 
 export default {
   createNodeView,
@@ -27,6 +26,5 @@ export default {
   WidgetView,
   newWidgetView,
   CodeBlockView,
-  HeadingView,
   FootnoteView,
 };
