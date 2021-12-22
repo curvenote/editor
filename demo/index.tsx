797bd484fe0d0115898473e52a2c8485d434d982
Yuxi Wang <yuxi.wangcd@gmail.com>
2021-12-22 14:01:50 -0700

    Remove unused code

--- demo/index.tsx
+++ demo/index.tsx
@@ -183,7 +183,7 @@ function Demo() {
           </AnchorBase>
           {/* <Editor stateKey={stateKey} viewId="two" /> */}
           <div className="sidenotes">
-            {comments.map(({ id, content, color }, index) => {
+            {comments.map(({ id }) => {
               return (
                 <Sidenote key={id} sidenote={id} base="anchor">
                   <div
@@ -191,18 +191,7 @@ function Demo() {
                       border: '1px grey solid',
                     }}
                   >
-                    <CommentEditor
-                      value={content}
-                      onChange={(v) => {
-                        console.log('comment changed', v);
-                        updateComments((c) => {
-                          const updated = { ...c[index], content: v };
-                          const cloned = [...c];
-                          cloned[index] = updated;
-                          return cloned;
-                        });
-                      }}
-                    />
+                    <CommentEditor />
                   </div>
                 </Sidenote>
               );
