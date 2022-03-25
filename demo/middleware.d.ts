a7c5252a433b63d015c5e889dcf9e7bcbe34b86a
rowanc1 <rowanc1@users.noreply.github.com>
2022-03-25 21:16:10 +0000

    deploy: d86026b8568bd5d2b7fe228ec4a59d2602e8540f

--- demo/middleware.d.ts
+++ demo/middleware.d.ts
@@ -1,2 +1,4 @@
-declare const _default: import("../src").Middleware[];
+declare const _default: ((import("redux-thunk").ThunkMiddleware<any, import("redux").AnyAction, undefined> & {
+    withExtraArgument<ExtraThunkArg, State = any, BasicAction extends import("redux").Action<any> = import("redux").AnyAction>(extraArgument: ExtraThunkArg): import("redux-thunk").ThunkMiddleware<State, BasicAction, ExtraThunkArg>;
+}) | import("../src").Middleware)[];
 export default _default;
