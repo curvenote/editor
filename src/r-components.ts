c1b39becbd8c944459cf425ac4da8d48d5eeb703
Yuxi Wang <yuxi.wangcd@gmail.com>
2022-06-20 19:45:51 -0700

    Add guard to prevent re-registering components

--- src/r-components.ts
+++ src/r-components.ts
@@ -2,6 +2,11 @@ import { types } from '@curvenote/runtime';
 import { register as basicRegister } from '@curvenote/components';
 import { Store } from './store/types';
 
+// acutally stamps WebComponents on to a global variable, if we don't guard the registeration we will need to deregister
+// TODO: instance editor properly...
+let isSetup = false;
 export default function setup(store: Store) {
+  if (isSetup) return;
   basicRegister(store as unknown as types.Store);
+  isSetup = true;
 }
