4d0836774ad13b8c9b8e67e922df911c91649785
Rowan Cockett <rowanc1@gmail.com>
2022-06-27 07:57:36 -0600

    🧹

--- src/r-components.ts
+++ src/r-components.ts
@@ -2,7 +2,7 @@ import { types } from '@curvenote/runtime';
 import { register as basicRegister } from '@curvenote/components';
 import { Store } from './store/types';
 
-// acutally stamps WebComponents on to a global variable, if we don't guard the registeration we will need to deregister
+// Stamps WebComponents onto a global variable, if we don't guard the registration we will need to deregister
 // TODO: instance editor properly...
 let isSetup = false;
 export default function setup(store: Store) {
