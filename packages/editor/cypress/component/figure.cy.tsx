b13911084e266b189a2c463fef9d63421c88de82
Yuxi Wang <yuxi.wangcd@gmail.com>
2022-09-21 15:37:54 -0700

    Fix figure tests

--- packages/editor/cypress/component/figure.cy.tsx
+++ packages/editor/cypress/component/figure.cy.tsx
@@ -1,18 +1,7 @@
 import React from 'react';
-import { render, act, assertElExists, delay } from './utils';
+import { render, waitFor } from './utils';
 import userEvent from '@testing-library/user-event';
 import { createStore, DemoEditor } from '../../demo/NewEditorDemo';
-import {
-  applyProsemirrorTransaction,
-  executeCommand,
-  getCurrentEditorState,
-  selectEditorView,
-} from '../../src/store/actions';
-import { CommandNames } from '../../src/store/suggestion/commands';
-import { TextSelection } from 'prosemirror-state';
-import { setTextSelection } from '@curvenote/prosemirror-utils';
-import { getEditorState } from '@curvenote/schema/dist/types/server';
-import { selectEditorState } from '../../src/store/selectors';
 
 // TODO: abstract these they are copy pasted from demo since the setup logic is shared
 const stateKey = 'myEditor';
@@ -43,7 +32,7 @@ const IMAGE_FIGURE_SNIPPET = generateFigureSnippet(IMAGE_CONTENT);
 const IFRAME_FIGURE_SNIPPET = generateFigureSnippet(IFRAME_CONTENT);
 
 it('should render curvenote link with the correct attributes', () => {
-  const { container, getAllByText } = render(<DemoEditor content={TABLE_FIGURE_SNIPPET} />);
+  const { container } = render(<DemoEditor content={TABLE_FIGURE_SNIPPET} />);
   expect(container.querySelector('table')).to.be.instanceOf(HTMLTableElement);
   expect(container.querySelector('figcaption')?.innerHTML).to.be.eq('this is caption!');
 });
@@ -51,14 +40,16 @@ it('should render curvenote link with the correct attributes', () => {
 [TABLE_FIGURE_SNIPPET, CODEBLOCK_FIGURE_SNIPPET, IMAGE_FIGURE_SNIPPET, IFRAME_FIGURE_SNIPPET].forEach((snippet) => {
   it('should create one new paragraph below the figure if hit enter within figure caption', async () => {
     const store = createStore();
-    const { container, getAllByText } = render(
+    const { container } = render(
       <DemoEditor content={snippet} store={store} stateKey={stateKey} viewId={viewId1} />,
     );
     const figcaption = container.querySelector('figcaption') as HTMLElement;
     userEvent.click(figcaption);
     await userEvent.keyboard('hello{Enter}{Enter}', { delay: 10 });
-    expect(
-      container.querySelectorAll('#first > div')[1]?.querySelectorAll(':scope > p').length, // there should be one and only one paragraph gets created;
-    ).to.eq(1);
+    await waitFor(() => {
+      expect(
+        container.querySelectorAll('#first > div')[1]?.querySelectorAll(':scope > p').length, // there should be one and only one paragraph gets created;
+      ).to.eq(1);
+    })
   });
 });
